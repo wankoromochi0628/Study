95b8cec2f8dd19cbf7a431c39e0c8d226f1fb3ad
yt_suzuki <>
2018-11-28 16:42:36 +0900

    circleCI 追加

--- WebpackBabel/src/index.js
+++ WebpackBabel/src/index.js
@@ -1,6 +1,30 @@
 // import {hello} from "./sub.js";
 
-// hello();
-var array1 = [1, 2, 3];
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log(array1.includes(2));
+let pGrp = [];
+let grp1 = [];
+let grp2 = [];
+
+while (array.length !== 0) {
+    pGrp.push(createGroup(array));
+    pGrp.push(createGroup(array));
+}
+
+console.log("pGrp");
+console.table(pGrp);
+console.table(array);
+
+export function createGroup(array) {
+    
+    let grp = [];
+
+    while (grp.length < 4) {
+        let num = Math.floor(Math.random() * array.length);
+        // grp1 = array.splice(Math.floor(Math.random() * array.length, 1));
+        grp.push(array.splice(num, 1));
+    }
+    return grp;
+}
+
+// console.log(array1.includes(2));
--- WebpackBabel/webpack.config.pro.js
+++ WebpackBabel/webpack.config.pro.js
@@ -1,3 +1,19 @@
 module.exports = {
-    mode: 'production'
+    mode: 'production',
+    entry: ['@babel/polyfill', './src/index.js'], // polyfill はIE11などで必要
+    output: {
+      path: `${__dirname}/dist`,
+      filename: 'main.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    }
   };
